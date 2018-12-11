5f643beaf490b39a1d5cf3b34081c88084c1b2a3
Edy <edy@simplus.sg>
2018-12-11 08:49:08 +0700

    Initial commit

---
+++ Testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by simplus on 12/11/18.
+//  Copyright © 2018 Simplus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
